f2c68022abb7a62eb14448aabf6174b1a2e0339b
elupus <elupus@xbmc.org>
2012-03-23 21:06:50 +0100

    Convert to avcodec_decode_audio4 and drop avcodec_decode_audio3.

--- lib/DllAvUtil.h
+++ lib/DllAvUtil.h
@@ -99,6 +99,7 @@ public:
   virtual int av_fifo_generic_write(AVFifoBuffer *f, void *src, int size, int (*func)(void*, void*, int)) = 0;
   virtual char *av_strdup(const char *s)=0;
   virtual int av_get_bytes_per_sample(enum AVSampleFormat p1) = 0;
+  virtual int av_samples_get_buffer_size (int *linesize, int nb_channels, int nb_samples, enum AVSampleFormat sample_fmt, int align) = 0;
 };
 
 #if (defined USE_EXTERNAL_FFMPEG)
@@ -131,6 +132,8 @@ public:
   virtual char *av_strdup(const char *s) { return ::av_strdup(s); }
   virtual int av_get_bytes_per_sample(enum AVSampleFormat p1)
     { return ::av_get_bytes_per_sample(p1); }
+  virtual int av_samples_get_buffer_size (int *linesize, int nb_channels, int nb_samples, enum AVSampleFormat sample_fmt, int align)
+    { return ::av_samples_get_buffer_size(linesize, nb_channels, nb_samples, sample_fmt, align); }
 
    // DLL faking.
    virtual bool ResolveExports() { return true; }
@@ -168,6 +171,7 @@ class DllAvUtilBase : public DllDynamic, DllAvUtilInterface
   DEFINE_METHOD4(int, av_fifo_generic_write, (AVFifoBuffer *p1, void *p2, int p3, int (*p4)(void*, void*, int)))
   DEFINE_METHOD1(char*, av_strdup, (const char *p1))
   DEFINE_METHOD1(int, av_get_bytes_per_sample, (enum AVSampleFormat p1))
+  DEFINE_METHOD5(int, av_samples_get_buffer_size, (int *p1, int p2, int p3, enum AVSampleFormat p4, int p5))
 
   public:
   BEGIN_METHOD_RESOLVE()
@@ -190,6 +194,7 @@ class DllAvUtilBase : public DllDynamic, DllAvUtilInterface
     RESOLVE_METHOD(av_fifo_generic_write)
     RESOLVE_METHOD(av_strdup)
     RESOLVE_METHOD(av_get_bytes_per_sample)
+    RESOLVE_METHOD(av_samples_get_buffer_size)
   END_METHOD_RESOLVE()
 };
 
