94b4d57f13fa55d922b09749230fb99baea04a90
elupus <elupus@xbmc.org>
2012-03-25 23:27:29 +0200

    Update ffmpeg to n0.10.2 (f139838d6473c7b5152178f602cb953a824c2ff9)
    xbmc ffmpeg 399301fd74ed748d24d7ee2806f50e8ad57aeea8

--- lib/ffmpeg/libavcodec/dxva2.c
+++ lib/ffmpeg/libavcodec/dxva2.c
@@ -21,13 +21,10 @@
  */
 
 #include "dxva2_internal.h"
-#include <unistd.h>
-
-#define MAX_RETRY_ON_PENDING		50
 
 void *ff_dxva2_get_surface(const Picture *picture)
 {
-    return picture->data[3];
+    return picture->f.data[3];
 }
 
 unsigned ff_dxva2_get_surface_index(const struct dxva_context *ctx,
@@ -91,17 +88,10 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, MpegEncContext *s,
     DXVA2_DecodeBufferDesc buffer[4];
     DXVA2_DecodeExecuteParams exec;
     int      result;
-    HRESULT  hr;
-    int      tries = 0;
 
-    while ((hr=IDirectXVideoDecoder_BeginFrame(ctx->decoder,
+    if (FAILED(IDirectXVideoDecoder_BeginFrame(ctx->decoder,
                                                ff_dxva2_get_surface(s->current_picture_ptr),
-                                               NULL)) == E_PENDING
-           && tries < MAX_RETRY_ON_PENDING) {
-        usleep(1000);
-        tries++;
-    }
-    if (FAILED(hr)) {
+                                               NULL))) {
         av_log(avctx, AV_LOG_ERROR, "Failed to begin frame\n");
         return -1;
     }
