afbedc25d3672b91fcefcba48d62af526a79b662
Franz Koch <franz.koch@elements-net.de>
2013-06-12 15:05:51 +0200

    [3D] add a builtin to change the stereomode

--- xbmc/guilib/StereoscopicsManager.h
+++ xbmc/guilib/StereoscopicsManager.h
@@ -46,6 +46,7 @@ public:
 
   void Initialize(void);
   void SetStereoMode(const RENDER_STEREO_MODE &mode);
+  void SetStereoMode(const std::string &mode);
   RENDER_STEREO_MODE GetStereoMode(void);
   RENDER_STEREO_MODE GetNextSupportedStereoMode(const RENDER_STEREO_MODE &currentMode, int step = 1);
   std::string DetectStereoModeByString(const std::string &needle);
